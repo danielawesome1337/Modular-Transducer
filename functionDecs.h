de14ecafba056a55311c4309dd7e39855000ab2d
danielawesome1337 <dsk13@ic.ac.uk>
2016-05-30 00:17:43 +0100

    v7

--- functionDecs.h
+++ functionDecs.h
@@ -1,8 +1,11 @@
-void pwmPhaser(unsigned int magRatio[8], unsigned int magPhase[8]);
+#ifndef _TRANSDUCER_COUNT
+#define TRANSDUCER_COUNT 8
+#endif
 
-void freqDrive(unsigned int t1, unsigned int t2, unsigned int halfWidth,
-        out buffered port:4 p, unsigned int pulseLength);
+void pwmPhaser(unsigned int magRatio[TRANSDUCER_COUNT], unsigned int magPhase[TRANSDUCER_COUNT]);
+
+void freqDrive(unsigned int t1, unsigned int t2, out buffered port:4 p, unsigned int pulseLength);
 
 [[combinable]]
-void pwmDrive(unsigned int magRatio, unsigned int flag, out buffered port:1 p,
-        unsigned int endTime, unsigned int pulseLength);
+ void pwmDrive(unsigned int magRatio,  unsigned int magPhase, out buffered port:1 p,
+         unsigned int endTime);
