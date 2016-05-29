31339ea68fdc7e9dcc15a4312e3d0b46bfcd6329
danielawesome1337 <dsk13@ic.ac.uk>
2016-05-30 00:03:23 +0100

    v3

--- functionDecs.h
+++ functionDecs.h
@@ -1,2 +1,4 @@
-void phaseMag(unsigned int phase, unsigned int halfWidth,
-        unsigned int magRatio, out buffered port:4 p);//pulseLength
+void freqDrive(unsigned int phase1, unsigned int phase2, unsigned int halfWidth, out buffered port:4 p);//pulseLength
+
+[[combinable]]
+void PWMdrive(unsigned int magRatio, out buffered port:1 p);
