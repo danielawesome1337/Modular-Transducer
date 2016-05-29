8d3a31b2052a68184cb53170e8e0d0935bf527f5
danielawesome1337 <dsk13@ic.ac.uk>
2016-05-30 00:22:25 +0100

    v7.5
    Finally fixed pulseLength problems - REF_RATE -> clockRate

--- functionDecs.h
+++ functionDecs.h
@@ -1,11 +1,13 @@
-#ifndef _TRANSDUCER_COUNT
+#ifndef TRANSDUCER_COUNT
 #define TRANSDUCER_COUNT 8
 #endif
 
-void pwmPhaser(unsigned int magRatio[TRANSDUCER_COUNT], unsigned int magPhase[TRANSDUCER_COUNT]);
+unsigned int binaryChop(unsigned int topRatio);
 
-void freqDrive(unsigned int t1, unsigned int t2, out buffered port:4 p, unsigned int pulseLength);
+void pwmPhaser(unsigned int magRatio[TRANSDUCER_COUNT], unsigned int magPhase[TRANSDUCER_COUNT/2]);
 
-[[combinable]]
- void pwmDrive(unsigned int magRatio,  unsigned int magPhase, out buffered port:1 p,
-         unsigned int endTime);
+void freqDrive(unsigned int t1, unsigned int t2, out buffered port:4 p, unsigned int pulseLength,
+        unsigned int pulseWait, unsigned int testLength, unsigned int burstLength, unsigned int burstWait, streaming chanend synchPulse);
+
+void pwmDrive(unsigned int magRatio1, unsigned int magRatio2, unsigned int magPhase,
+        out buffered port:1 p1, out buffered port:1 p2, streaming chanend synchPulse);
