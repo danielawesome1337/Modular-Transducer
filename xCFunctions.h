11ca92e4d41b58c3d14290561d6de8bfb8daefc8
danielawesome1337 <dsk13@ic.ac.uk>
2016-06-04 09:29:47 +0100

    v10

--- xCFunctions.h
+++ xCFunctions.h
@@ -1,3 +1,10 @@
+#ifndef SYS_HEADERS
+#include <stdint.h>
+#endif
+
+#ifndef FUNCTIONDECS_H_
+#define FUNCTIONDECS_H_
+
 #ifndef TRANSDUCER_COUNT
 #define TRANSDUCER_COUNT 8
 #endif
@@ -8,8 +15,8 @@
 //struct definitions
 typedef struct
 {
-    //topRatio is the ideal frequency of pp
-    unsigned int topRatio;
+    //topRatio is the frequency of pp
+    unsigned int frequency;
     //testLength is number of bursts in the test
     short testLength;
     //burstLength is the number of pulses per burst
@@ -42,11 +49,11 @@ void dataCapture(unsigned int data[DATA_LENGTH]);
 
 short pwmPhaser(pwmStruct pwmD1, pwmStruct pwmD2);
 
-unsigned int binaryChop(unsigned int topRatio);
-
-void clockConfig(unsigned int topRatio);
+void clkSet();
 
 void pwmDrive(pwmStruct pwmD1, pwmStruct pwmD2,
         out buffered port:1 p1, out buffered port:1 p2, streaming chanend synchPulse);
 
 void freqDrive(ppStruct ppD1, ppStruct ppD2, out buffered port:4 p, streaming chanend synchPulse);
+
+#endif /* FUNCTIONDECS_H_ */
