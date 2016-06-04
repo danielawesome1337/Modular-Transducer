afcdb13fceb2dcf2c6924352220eb652bed9cb52
danielawesome1337 <dsk13@ic.ac.uk>
2016-06-04 09:27:08 +0100

    v12

--- xcFunctions.h
+++ xcFunctions.h
@@ -1,15 +1,22 @@
+#ifndef SYS_HEADERS
+#include <stdint.h>
+#endif
+
+#ifndef FUNCTIONDECS_H_
+#define FUNCTIONDECS_H_
+
 #ifndef TRANSDUCER_COUNT
 #define TRANSDUCER_COUNT 8
 #endif
 #ifndef DATA_LENGTH
-#define DATA_LENGTH 22
+#define DATA_LENGTH 28
 #endif
 
 //struct definitions
 typedef struct
 {
-    //topRatio is the ideal frequency of pp
-    unsigned int topRatio;
+    //topRatio is the frequency of pp
+    unsigned int frequency;
     //testLength is number of bursts in the test
     short testLength;
     //burstLength is the number of pulses per burst
@@ -26,6 +33,13 @@ typedef struct
     unsigned int burstWait;
     //pulseWait is how many ticks to wait after each pulse at clockRate
     unsigned int pulseWait;
+    //Adafruit SI5351 clock generation parameters
+    uint8_t PLLmult;
+    uint32_t PLLnum;
+    uint32_t PLLdenom;
+    uint32_t multisynthDiv;
+    uint32_t multisynthNum;
+    uint32_t multisynthDenom;
 } ppStruct;
 
 typedef struct
@@ -40,11 +54,15 @@ void dataProcessor(ppStruct ppD[TRANSDUCER_COUNT], pwmStruct pwmD[TRANSDUCER_COU
 
 void dataCapture(unsigned int data[DATA_LENGTH]);
 
+void adaSet(client i2c_master_if i2c, ppStruct ppD);
+
 short pwmPhaser(pwmStruct pwmD1, pwmStruct pwmD2);
 
-void clockConfig(unsigned int topRatio);
+void clkSet();
 
 void pwmDrive(pwmStruct pwmD1, pwmStruct pwmD2,
         out buffered port:1 p1, out buffered port:1 p2, streaming chanend synchPulse);
 
 void freqDrive(ppStruct ppD1, ppStruct ppD2, out buffered port:4 p, streaming chanend synchPulse);
+
+#endif /* FUNCTIONDECS_H_ */
