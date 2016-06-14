fc560771788139cd078f762c2e34d8fb47786858
danielawesome1337 <dsk13@ic.ac.uk>
2016-06-14 22:42:16 +0100

    Update runMaster.bat

--- runMaster.bat
+++ runMaster.bat
@@ -2,10 +2,10 @@
 @echo off
 
 :: add Adafruit params to parameters.txt
-start /wait clockGenParams
+start /wait clockGenParams.exe
 
 :: setup environment
-call "D:\Program Files (x86)\XMOS\Community_14.1.2\Community_14.2.0\SetEnv.bat"
+call "C:\Program Files (x86)\XMOS\xTIMEcomposer\Community_14.1.2\SetEnv.bat"
 
 :: run binary.xe
 xrun --id 0 --io 1\master.xe
